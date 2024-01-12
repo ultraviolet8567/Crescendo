1269da80946b43fa7b0084d2ec73e7f2534a3206
chaerin <cherryn.shin@gmail.com>
2024-01-12 15:58:01 -0500

    remove auto-generated code & test pipeline

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
@@ -15,8 +14,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-    private Command m_autonomousCommand;
-
     private RobotContainer m_robotContainer;
 
     /**
@@ -53,15 +50,16 @@ public class Robot extends TimedRobot {
     @Override
     public void disabledPeriodic() {}
 
+    // uncomment below code if autonomous is ready to be developed
     /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         // schedule the autonomous command (example)
-        if (m_autonomousCommand != null) {
-        m_autonomousCommand.schedule();
-        }
+        // if (m_autonomousCommand != null) {
+        // m_autonomousCommand.schedule();
+        // }
     }
 
     /** This function is called periodically during autonomous. */
@@ -74,9 +72,10 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (m_autonomousCommand != null) {
-        m_autonomousCommand.cancel();
-        }
+
+        // if (m_autonomousCommand != null) {
+        // m_autonomousCommand.cancel();
+        // }
     }
 
     /** This function is called periodically during operator control. */
