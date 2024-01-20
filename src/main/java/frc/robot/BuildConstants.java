79a54a42565aa19b1abb5839e576ca6ae8b4c13a
Jai <rajthetaj122@gmail.com>
2024-01-20 14:41:20 -0500

    Co-authored-by: KavanBelani <KavanBelani@users.noreply.github.com> Co-authored-by: njv0 <njv0@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 6;
-	public static final String GIT_SHA = "8ddbdb941578d71b768b69e6d674f9616b972a3c";
-	public static final String GIT_DATE = "2024-01-12 16:05:19 EST";
-	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2024-01-13 22:28:24 EST";
-	public static final long BUILD_UNIX_TIME = 1705202904523L;
+	public static final int GIT_REVISION = 10;
+	public static final String GIT_SHA = "fe28f00a64d7852bd89641fb8c4d012c706c7dab";
+	public static final String GIT_DATE = "2024-01-19 17:34:09 EST";
+	public static final String GIT_BRANCH = "basic";
+	public static final String BUILD_DATE = "2024-01-20 14:39:39 EST";
+	public static final long BUILD_UNIX_TIME = 1705779579209L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
