6418df23184432bc57d32ac9af373d4f919479a1
infernalsquid <arunsiddharth567@gmail.com>
2024-02-25 18:28:08 -0500

    downgraded photonlib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 105;
-	public static final String GIT_SHA = "1a42258b63b2d619b33eaa74bd1e321f2d8f9b9e";
-	public static final String GIT_DATE = "2024-02-22 19:56:03 EST";
+	public static final int GIT_REVISION = 112;
+	public static final String GIT_SHA = "da90ac9f66c5daed1ebc1617083cc187bff0fe9d";
+	public static final String GIT_DATE = "2024-02-25 17:10:32 EST";
 	public static final String GIT_BRANCH = "dev";
-	public static final String BUILD_DATE = "2024-02-22 20:03:13 EST";
-	public static final long BUILD_UNIX_TIME = 1708650193104L;
+	public static final String BUILD_DATE = "2024-02-25 18:08:52 EST";
+	public static final long BUILD_UNIX_TIME = 1708902532109L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
