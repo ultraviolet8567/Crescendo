328fba527e59510d7b37fe82c8a11d74617f94b6
h0gs <happylittlepiggy13@gmail.com>
2024-03-04 17:10:04 -0500

    Co-authored-by: njv0 <njv0@users.noreply.github.com> Co-authored-by: IncrediblyCreativeUsername <IncrediblyCreativeUsername@users.noreply.github.com> Co-authored-by: murham <murham@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -5,7 +5,10 @@ import static frc.robot.Constants.GainsConstants.shooterGains;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.intake.Intake;
 import frc.robot.util.LoggedTunableNumber;
+import java.util.Timer;
+import java.util.TimerTask;
 import org.littletonrobotics.junction.Logger;
 
 public class Shooter extends SubsystemBase {
@@ -16,16 +19,19 @@ public class Shooter extends SubsystemBase {
 	private static final LoggedTunableNumber kV = new LoggedTunableNumber("Shooter/kV", shooterGains.ffkV());
 
 	private final Arm arm;
+	private final Intake intake;
 	private final ShooterIO io;
 	private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+	Timer timer = new Timer();
 
 	/*
 	 * Initialize all components here, as well as any one-time logic to be completed
 	 * on boot-up
 	 */
-	public Shooter(ShooterIO io, Arm arm) {
+	public Shooter(ShooterIO io, Arm arm, Intake intake) {
 		this.io = io;
 		this.arm = arm;
+		this.intake = intake;
 	}
 
 	/* Runs periodically (about once every 20 ms) */
@@ -44,6 +50,20 @@ public class Shooter extends SubsystemBase {
 	public void shoot() {
 		double targetVel = getTargetVelocity();
 		io.setVelocity(targetVel, targetVel);
+		// 87% buffer if the shooter flywheels never actually reach their target
+		// velocity
+		if (inputs.topVelocityRPM >= 0.87 * targetVel && inputs.bottomVelocityRPM >= 0.87 * targetVel) {
+			intake.runIndexer();
+			if (!intake.noteDetected) {
+				timer.schedule(new TimerTask() {
+					@Override
+					public void run() {
+						io.stop();
+					}
+				}, 1000);
+			}
+		}
+
 	}
 
 	public double getTargetVelocity() {
