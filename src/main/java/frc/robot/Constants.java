5370a2a49f87e68aef6671dd59acc345feb0fb40
Kaloyan Draganov <kaloyan@draganov.com>
2024-03-06 13:08:20 -0500

    Create flywheel velocity threshold

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -194,6 +194,8 @@ public final class Constants {
 	}
 
 	public static final class ShooterConstants {
+		public static final double kVelocityThreshold = 0.8;
+
 		public static final double kShooterReduction = 1.0;
 
 		public static final LoggedTunableNumber kShooterPIDTolerance = new LoggedTunableNumber("Shooter/PID Tolerance",
