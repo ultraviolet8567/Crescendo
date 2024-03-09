67f7aa21188deced6952da321435d227e08734e4
TaoTaoBamBam <srta.taotaobambam@gmail.com>
2024-03-09 16:48:42 -0500

    added 3 note rush source auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 152;
-	public static final String GIT_SHA = "2b29bc8f0f5f8345ecd0062b1dbb2180da496c5b";
-	public static final String GIT_DATE = "2024-03-09 13:09:46 EST";
+	public static final int GIT_REVISION = 154;
+	public static final String GIT_SHA = "bb46f5b353880a2b6e7b7f981a5975514bb2f589";
+	public static final String GIT_DATE = "2024-03-09 14:52:42 EST";
 	public static final String GIT_BRANCH = "bridgewater";
-	public static final String BUILD_DATE = "2024-03-09 13:31:45 EST";
-	public static final long BUILD_UNIX_TIME = 1710009105137L;
+	public static final String BUILD_DATE = "2024-03-09 16:11:32 EST";
+	public static final long BUILD_UNIX_TIME = 1710018692861L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
