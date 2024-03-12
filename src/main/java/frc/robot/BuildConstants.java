b62292680569d090ccee840e6111019b8060d19a
Kaloyan Draganov <kaloyan@draganov.com>
2024-03-12 13:30:01 -0400

    Make `AutoChooser` a `VirtualSubsystem`

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 160;
-	public static final String GIT_SHA = "7e33a62df0e1ff565a7f1cf47b4ddaf125107b5b";
-	public static final String GIT_DATE = "2024-03-10 20:24:39 EDT";
+	public static final int GIT_REVISION = 164;
+	public static final String GIT_SHA = "d8c7d6c3f204a9841dddad536e265994483ca2e6";
+	public static final String GIT_DATE = "2024-03-12 00:10:03 EDT";
 	public static final String GIT_BRANCH = "bridgewater";
-	public static final String BUILD_DATE = "2024-03-10 20:25:00 EDT";
-	public static final long BUILD_UNIX_TIME = 1710116700957L;
-	public static final int DIRTY = 0;
+	public static final String BUILD_DATE = "2024-03-12 13:20:08 EDT";
+	public static final long BUILD_UNIX_TIME = 1710264008113L;
+	public static final int DIRTY = 1;
 
 	private BuildConstants() {
 	}
--- src/main/java/frc/robot/subsystems/AutoChooser.java
+++ src/main/java/frc/robot/subsystems/AutoChooser.java
@@ -4,18 +4,14 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.VirtualSubsystem;
 import org.littletonrobotics.junction.Logger;
 
-public class AutoChooser extends SubsystemBase {
+public class AutoChooser extends VirtualSubsystem {
 	private static final ShuffleboardTab main = Shuffleboard.getTab("Main");
-
-	private final SendableChooser<String> sideOfField;
-	private final SendableChooser<String> noteNumber;
-	private final SendableChooser<String> otherStuff;
+	private final SendableChooser<String> sideOfField, noteNumber, otherStuff;
 
 	public AutoChooser() {
-		// Post the selectors to the ShuffleBoard
 		noteNumber = new SendableChooser<>();
 		noteNumber.setDefaultOption("0 Note", "Do Nothing");
 		noteNumber.addOption("1 Note", "1 Note");
@@ -23,37 +19,44 @@ public class AutoChooser extends SubsystemBase {
 		noteNumber.addOption("3 Note", "3 Note");
 		noteNumber.addOption("4 Note", "4 Note");
 		noteNumber.addOption("5 Note", "5 Note");
-		main.add("Number of Notes", noteNumber).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
-				.withPosition(3, 0);
 
 		sideOfField = new SendableChooser<>();
 		sideOfField.setDefaultOption("Amp", "Amp Side");
 		sideOfField.addOption("Source", "Source Side");
 		sideOfField.addOption("Center", "Center");
-		main.add("Side of Field", sideOfField).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
-				.withPosition(3, 1);
 
 		otherStuff = new SendableChooser<>();
 		otherStuff.setDefaultOption("None", "");
+		otherStuff.addOption("Test", "Test");
 		otherStuff.addOption("Rush", "Rush ");
 		otherStuff.addOption("Inner", "Inner ");
 		otherStuff.addOption("Out of Way", "Out Of Way ");
-		otherStuff.addOption("Test", "Test");
 		otherStuff.addOption("Don't Move", "Don't Move ");
-		main.add("Other variables", otherStuff).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
+
+		// Post the selectors to the ShuffleBoard
+		main.add("Number of Notes", noteNumber).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
+				.withPosition(3, 0);
+		main.add("Side of Field", sideOfField).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
+				.withPosition(3, 1);
+		main.add("Other Variables", otherStuff).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 1)
 				.withPosition(3, 2);
 	}
 
-	public String getAutoCommand() {
-		String routineName = noteNumber.getSelected() + " " + otherStuff.getSelected() + sideOfField.getSelected();
-		Logger.recordOutput("Auto/Routine", routineName);
+	@Override
+	public void periodic() {
+		Logger.recordOutput("Auto/Routine", getAutoCommand());
+	}
 
+	/**
+	 * Returns name of pre-defined autonomous command based on Shuffleboard input
+	 */
+	public String getAutoCommand() {
 		if (noteNumber.getSelected().equals("Do Nothing")) {
 			return "Do Nothing";
 		} else if (otherStuff.getSelected().equals("Test")) {
 			return "Test";
 		} else {
-			return routineName;
+			return noteNumber.getSelected() + " " + otherStuff.getSelected() + sideOfField.getSelected();
 		}
 	}
 }
