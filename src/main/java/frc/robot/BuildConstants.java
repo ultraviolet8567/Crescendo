bccd2d5e07d5abfc8bd1878c3ee771a2a79887af
Kaloyan Draganov <kaloyan@draganov.com>
2024-03-12 19:30:06 -0400

    Change to logging on USB thumb drive

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 164;
-	public static final String GIT_SHA = "d8c7d6c3f204a9841dddad536e265994483ca2e6";
-	public static final String GIT_DATE = "2024-03-12 00:10:03 EDT";
-	public static final String GIT_BRANCH = "bridgewater";
-	public static final String BUILD_DATE = "2024-03-12 13:20:08 EDT";
-	public static final long BUILD_UNIX_TIME = 1710264008113L;
+	public static final int GIT_REVISION = 174;
+	public static final String GIT_SHA = "1c0c06eb7c5aaaa4ac4eb0fde1726090d34bf59b";
+	public static final String GIT_DATE = "2024-03-12 16:40:22 EDT";
+	public static final String GIT_BRANCH = "dev";
+	public static final String BUILD_DATE = "2024-03-12 18:28:41 EDT";
+	public static final long BUILD_UNIX_TIME = 1710282521255L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends LoggedRobot {
 
 		switch (Constants.currentMode) {
 			case REAL :
-				Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs/"));
+				Logger.addDataReceiver(new WPILOGWriter());
 				Logger.addDataReceiver(new NT4Publisher());
 				break;
 			case SIM :
