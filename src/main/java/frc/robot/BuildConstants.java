a3b8c4fc85b9991db954e9befdcccc23dbcc0889
Kaloyan Draganov <kaloyan@draganov.com>
2024-03-13 10:04:48 -0400

    Update vendor libraries

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 174;
-	public static final String GIT_SHA = "1c0c06eb7c5aaaa4ac4eb0fde1726090d34bf59b";
-	public static final String GIT_DATE = "2024-03-12 16:40:22 EDT";
+	public static final int GIT_REVISION = 179;
+	public static final String GIT_SHA = "bccd2d5e07d5abfc8bd1878c3ee771a2a79887af";
+	public static final String GIT_DATE = "2024-03-12 19:30:06 EDT";
 	public static final String GIT_BRANCH = "dev";
-	public static final String BUILD_DATE = "2024-03-12 18:28:41 EDT";
-	public static final long BUILD_UNIX_TIME = 1710282521255L;
+	public static final String BUILD_DATE = "2024-03-13 09:58:51 EDT";
+	public static final long BUILD_UNIX_TIME = 1710338331554L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
