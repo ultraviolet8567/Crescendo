c959e1ffeb16c81edc69e1e9fb400e9f8ff97e19
Kaloyan Draganov <kaloyan@draganov.com>
2024-04-01 11:50:11 -0400

    Update vendordeps and WPILib Tools settings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "Crescendo";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 216;
-	public static final String GIT_SHA = "c911a2fa180504b8c5bdb52c0eea5c4a8cbfedc0";
-	public static final String GIT_DATE = "2024-03-29 15:45:38 EDT";
+	public static final int GIT_REVISION = 218;
+	public static final String GIT_SHA = "23fd27f27fa3f285a5a30394ceae543f67265a58";
+	public static final String GIT_DATE = "2024-04-01 11:41:58 EDT";
 	public static final String GIT_BRANCH = "dev";
-	public static final String BUILD_DATE = "2024-04-01 11:34:31 EDT";
-	public static final long BUILD_UNIX_TIME = 1711985671045L;
+	public static final String BUILD_DATE = "2024-04-01 11:49:17 EDT";
+	public static final long BUILD_UNIX_TIME = 1711986557717L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
